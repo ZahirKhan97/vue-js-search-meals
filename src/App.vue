1e054f0d22eccfe817143fc25c9fa129b6ef1a60
MZK <139211996+ZahirKhan97@users.noreply.github.com>
2025-04-29 11:53:16 +0500

    vue-js-meals-search

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <router-link to="/">Home</router-link> -->
+  <router-view> </router-view>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+<style scoped>
+
+</style>
